30923ad783b02f22604e0bfbd58fe132f9ee1e8b
Tuan Le <letuan317@gmail.com>
2021-12-24 13:10:47 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "style/App.scss";
+
+import Navigation from "common/sharedComponents/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/sharedComponents/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <div className="nav">
+      <a hreff="#">Home</a>
+      <a hreff="#">TV Shows</a>
+      <a hreff="#">Movies</a>
+      <a hreff="#">Latest</a>
+    </div>
+  );
+}
